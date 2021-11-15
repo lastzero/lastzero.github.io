7b9c72e2151ece2cd9aabf65cad7a8e605304a9f
Michael Mayer <michael@photoprism.app>
2021-11-15 16:46:34 +0100

    Lean, business card-like site

--- js/init.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  $(function(){
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $('a[href="' + window.location.pathname + '"]').parents().addClass('active');
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
