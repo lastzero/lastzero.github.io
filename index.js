79ef4008b55272cc0d218736120387778ebdbc41
Michael Mayer <michael@photoprism.app>
2022-02-02 16:39:43 +0100

    Update Site HTML, JS & CSS

---
+++ index.js
@@ -0,0 +1,52 @@
+// Source: https://codepen.io/P3R0/pen/MwgoKv
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+
+//making the canvas full screen
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+
+// characters - taken from the unicode charset
+var charset = "LIQUIDBYTES";
+
+//converting the string into an array of single characters
+charset = charset.split("");
+
+var font_size = 10;
+var columns = c.width/font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+  drops[x] = 1;
+
+//drawing the characters
+function draw()
+{
+  //Black BG for the canvas
+  //translucent BG to show trail
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, c.width, c.height);
+
+  ctx.fillStyle = "#66bdeb"; //green text
+  ctx.font = font_size + "px monospace";
+  //looping over drops
+  for(var i = 0; i < drops.length; i++)
+  {
+    //a random chinese character to print
+    var text = charset[Math.floor(Math.random()*charset.length)];
+    //x = i*font_size, y = value of drops[i]*font_size
+    ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+    //sending the drop back to the top randomly after it has crossed the screen
+    //adding a randomness to the reset to make the drops scattered on the Y axis
+    if(drops[i]*font_size > c.height && Math.random() > 0.975)
+      drops[i] = 0;
+
+    //incrementing Y coordinate
+    drops[i]++;
+  }
+}
+
+setInterval(draw, 33);
